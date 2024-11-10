ede766b6f4c91319663a966328cf3fe24a9e57e4
GijsW03 <gijsworm03@gmail.com>
2024-11-10 21:32:06 +0100

    cehck collision veriwjderd in updatenemies omdat t toch niet werkte lol

--- src/Enemy.hs
+++ src/Enemy.hs
@@ -5,7 +5,6 @@ module Enemy where
 import Model
 import System.Random (Random(randomR))
 
-
 moveEnemy :: Float -> GameState -> Enemy -> Enemy
 moveEnemy secs gstate enemy = enemy { enemyPosition = (x + enemySpeedX * secs, y + enemySpeedY) }
   where (x, y) = enemyPosition enemy
@@ -42,20 +41,25 @@ updateEnemies secs gstate = filter alive . map (updateEnemy secs gstate)
       | otherwise = 
           let 
               enemy' = moveEnemy secs gstate enemy
-              (enemyHit, remainingProjectiles) = checkCollision enemy' (projectiles (player gstate))
-              updatedEnemy = if enemyHit then enemy' { enemyHealth = enemyHealth enemy' - 1 } else enemy'
-          in if enemyHit && enemyHealth updatedEnemy <= 0
+              updatedEnemy = if isHit enemy' (projectiles (player gstate))
+                             then enemy' { enemyHealth = enemyHealth enemy' - 1 }
+                             else enemy'
+          in if enemyHealth updatedEnemy <= 0
              then checkDeath updatedEnemy  -- Enemy is defeated, remove it
              else updatedEnemy
-      where
-        -- Check for collision and filter out hit projectiles (this doesn't work sadly)  
-        checkCollision enemy = foldr (\proj (hit, projs) ->
-          if isCollision proj enemy then (True, projs) else (hit, proj : projs)) (False, [])
 
+    -- Function to trigger the death animation or mark the enemy as dead
     checkDeath enemy = case deathAnimationTimer enemy of 
       Just progress | progress < 10 -> enemy { deathAnimationTimer = Just (progress + 0.1)} 
       Just _                        -> enemy { isDead = True }
       Nothing                       -> enemy { deathAnimationTimer = Just 0 }
 
+    -- Function to check if an enemy is still within the game bounds
     inBounds enemy = fst (enemyPosition enemy) >= -500
+    
+    -- Check if enemy is still alive (not dead)
     alive enemy = not (isDead enemy)
+    
+    -- Function to check if an enemy is hit by any projectile
+    isHit enemy projectiles = any (`isCollision` enemy) projectiles
+
